9b6f4bf4c0d0d2330253a4c5a33ceed4e5bfc96b
Nick Cherry <nickcherryjiggz@gmail.com>
2022-05-13 10:52:46 -0400

    add example

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
+import React, { FC } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
+const App: FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <WebView
+        source={{
+          uri: 'https://imaginative-halva-7ad92c.netlify.app',
+        }}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+export default App;
